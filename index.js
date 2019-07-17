df2b79751ec119c9a7ec5c1732b8ab79f6d6adfa
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-17 14:36:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  // it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object'
 }
 
 function deleteFromObjectByKey(object, key) {
